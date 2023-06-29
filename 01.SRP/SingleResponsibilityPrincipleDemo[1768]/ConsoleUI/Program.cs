074ca99caa4e235a00eaad735a25ea4bcfa45d83
Erikato <erik.kato@hotmail.com>
2023-06-29 22:12:02 +0200

    SOLID

---
+++ 01.SRP/SingleResponsibilityPrincipleDemo[1768]/ConsoleUI/Program.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StandardMessages.WelcomeMessage();
+
+            Person user = PersonDataCapture.Capture();
+
+            bool isUserValid = PersonValidator.Validate(user);
+
+            if (isUserValid == false)
+            {
+                StandardMessages.EndApplication();
+                return;
+            }
+
+            AccountGenerator.CreateAccount(user);
+
+            StandardMessages.EndApplication();
+        }
+    }
+}
+
+// A class should only have one responsibility
+
+
+
+
+//namespace ConsoleUI
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Welcome to my application!");
+
+//            Person user = new Person();
+
+//            Console.WriteLine("What is your first name");
+//            user.FirstName = Console.ReadLine();
+
+//            Console.WriteLine("What is your last name");
+//            user.LastName = Console.ReadLine();
+
+//            if (string.IsNullOrWhiteSpace(user.FirstName))
+//            {
+//                Console.WriteLine("You did not give a valid first name");
+//                Console.ReadLine();
+//                return;
+//            }
+
+//            if (string.IsNullOrWhiteSpace(user.LastName))
+//            {
+//                Console.WriteLine("You did not give a valid last name");
+//                Console.ReadLine();
+//                return;
+//            }
+
+//            Console.WriteLine("Your user name is: {0} {1}", user.FirstName, user.LastName);
+
+//            Console.ReadLine();    
+//        }
+//    }
+//}
+
+
---
+++ 03.LSP/LiskovSubstitutionPrincipleDemo/LSPDemo_End/ConsoleUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        // Classes should not depend on properties in interfaces they don't implement, e.g. CEO doesn't have an AssignManager()
+        static void Main(string[] args)
+        {
+            IManager accountingVP = new Manager();
+
+            accountingVP.FirstName = "Emma";
+            accountingVP.LastName = "Stone";
+            accountingVP.CalculatePerHourRate(4);
+
+            IManaged emp = new Manager();
+
+            emp.FirstName = "Tim";
+            emp.LastName = "Corey";
+            emp.AssignManager(accountingVP);
+            emp.CalculatePerHourRate(2);
+
+            Console.WriteLine($"{ emp.FirstName }'s salary is ${ emp.Salary }/hour.");
+
+            Console.ReadLine();
+        }
+    }
+}
